e8bfe5c97653ed98a9028e8b2aad2cc94f1394ca
André Luis <andrellui12345@gmail.com>
2024-03-01 20:03:35 -0400

    feat: add new header in pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,58 @@
-import React from "react";
+import * as React from "react";
 import { NavLink } from "react-router-dom";
-import { AppBar, Box, Toolbar } from "@mui/material";
+import { Box, AppBar, Toolbar, Container, Typography } from "@mui/material";
+
 
 function Header() {
   return (
-    <header>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
+    <div>
+      <AppBar
+        position="fixed"
+        sx={{
+          boxShadow: 0,
+          bgcolor: "transparent",
+          backgroundImage: "none",
+          mt: 2,
+        }}
+      >
+        <Container maxWidth="lg">
+          <Toolbar
+            variant="regular"
+            sx={() => ({
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              flexShrink: 0,
+              borderRadius: "999px",
+              bgcolor: "#002C4B",
+              backdropFilter: "blur(24px)",
+              maxHeight: 40,
+              border: "1px solid",
+              borderColor: "divider",
+            })}
+          >
             <NavLink to="/">
               <button className="flex">
                 <i className="material-icons">arrow_back</i>
               </button>
             </NavLink>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                px: 0,
+              }}
+            >
+              <Typography component="h1" variant="h5" className="animate-fade-left animate-delay-200">
+                Cash On Delivery
+              </Typography>
+            </Box>
           </Toolbar>
-        </AppBar>
-      </Box>
-    </header>
+        </Container>
+      </AppBar>
+    </div>
   );
 }
 
